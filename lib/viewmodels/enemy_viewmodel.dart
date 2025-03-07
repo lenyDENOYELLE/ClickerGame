22c71dfd1c340597427ad456c95e924e55704d46
lenyDENOYELLE <lenydenoyelle@gmail.com>
2025-03-07 11:15:33 +0100

    Ajout des classes pour EnemyViewModel

---
+++ lib/viewmodels/enemy_viewmodel.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:untitled1/core/services/enemy_service.dart';
+import 'package:untitled1/exceptions/levelnotfound_exception.dart';
+import 'package:untitled1/models/enemy.dart';
+
+class EnemyViewModel extends ChangeNotifier {
+  final EnemyService _enemyRequest = EnemyService();
+  late EnemyModel _enemy;
+  bool fetchNewEnemy = true;
+  int _level = 1;
+
+  int get level => _enemy.level;
+  int get totalLife => _enemy.totalLife;
+  int get currentLife => _enemy.currentLife;
+
+  void nextEnemy(){
+    _level++;
+    fetchEnemy();
+  }
+
+  Future<bool> fetchEnemy() async{
+    EnemyModel? enemytrouve = await _enemyRequest.getEnemyById(_level);
+
+      if (enemytrouve != null){
+        _enemy = enemytrouve;
+        return true;
+      }
+      else{
+        fetchNewEnemy = false;
+        throw LevelNotFoundException(_level);
+      }
+  }
+
+  void attackEnemy(int damage){
+    _enemy.reduceLife(damage);
+    if (_enemy.currentLife <= 0){
+      _level++;
+    }
+  }
+
+
+}
