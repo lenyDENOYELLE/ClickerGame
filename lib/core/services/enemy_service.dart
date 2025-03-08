78167c72388ccc0b52c012d02aef9615ba516341
Ydox25 <ybenouda@iut.univ-paris8.fr>
2025-03-08 17:46:24 +0100

    ajout des fichiers get et post dans le dossier api (sauf post_buy) et changement des images

--- lib/core/services/enemy_service.dart
+++ lib/core/services/enemy_service.dart
@@ -8,10 +8,10 @@ class EnemyService {
   //final ApiService apiService = ApiService();
   final List<EnemyModel> _sansApi = [
     EnemyModel(name: 'blue slime', level: 1, totalLife: 10),
-    EnemyModel(name: 'green slime', level: 2, totalLife: 20),
-    EnemyModel(name: 'red slime', level: 3, totalLife: 30),
-    EnemyModel(name: 'yellow slime', level: 4, totalLife: 40),
-    EnemyModel(name: 'king slime', level: 5, totalLife: 50),
+    EnemyModel(name: 'bat', level: 2, totalLife: 20),
+    EnemyModel(name: 'king slime', level: 3, totalLife: 30),
+    EnemyModel(name: 'golem', level: 4, totalLife: 40),
+    EnemyModel(name: 'dragon', level: 5, totalLife: 50),
   ];
 
   Future<EnemyModel?> getEnemyById(int id) async {
